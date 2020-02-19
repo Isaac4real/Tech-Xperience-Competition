9357143705df2608a05bb9846eac4999ae243c6f
Isaac Bastos Correia Reis <isaac.reis@sonaearauco.com>
2020-02-19 22:44:17 +0000

    First trained model

---
+++ ML_App/ML_App/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ML_AppML.Model;
+
+namespace ML_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Add input data
+            var input = new ModelInput();
+            input.ImageSource = "../../../../Dataset/shaver/image10.jpg";
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine($"img: {input.ImageSource}\n Is : {result.Prediction}");
+        }
+    }
+}
