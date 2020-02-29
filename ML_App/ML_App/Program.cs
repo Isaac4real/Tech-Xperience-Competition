acd893a2d85066d1f797f9f88f63bb649ee84bbc
Isaac Bastos Correia Reis <isaac.reis@sonaearauco.com>
2020-02-29 14:46:31 +0000

    Fixed paths

--- ML_App/ML_App/Program.cs
+++ ML_App/ML_App/Program.cs
@@ -12,15 +12,18 @@ namespace ML_App
             ConsoleHelpers.ConsoleWriteHeader("Welcome to the A.I. Classifier, Philips!");
 
             // Input's Images Folder
-            string ImageSourceRelativePath = @"Input/";
+            /* IF BUILDING NORMALLY */ string ImageSourceRelativePath = @"../../../../Input/";
+            /* IF BUILDING DOCKER IMAGE USE THIS *///string ImageSourceRelativePath = @"Input/";
             string ImageSource = Path.GetFullPath(ImageSourceRelativePath);
 
             // ML Model Path
-            string modelRelativePath = @"MLModel/MLModel.zip";
+            /* IF BUILDING NORMALLY */ string modelRelativePath = @"../../../MLModel/MLModel.zip";
+            /* IF BUILDING DOCKER IMAGE USE THIS *///string modelRelativePath = @"MLModel/MLModel.zip";
             string modelPath = Path.GetFullPath(modelRelativePath);
 
             // Where the .txt file will be written
-            string OutputTxtSourceRelativePath = @"Output/";
+            /* IF BUILDING NORMALLY */ string OutputTxtSourceRelativePath = @"../../../../Output/";
+            /* IF BUILDING DOCKER IMAGE USE THIS *///string OutputTxtSourceRelativePath = @"Output/";
             string OutputTxtSource = Path.GetFullPath(OutputTxtSourceRelativePath);
 
             try
